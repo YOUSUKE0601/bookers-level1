31bf6c159b711165b71fb2fd1d15a279b86ecb42
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-02-14 06:34:05 +0000

    [Finish]課題②完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,12 +14,13 @@ class BooksController < ApplicationController
 
   
   def create
-    book = Book.new(book_params)
-    if book.save
-      flash[:notice] = "successfully"
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
-      render :new
+      render action: :index
     end
   end
 
@@ -28,19 +29,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      flash[:notice] = "successfully"
-      redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book.id)
     else
-      render :new
+      render action: :edit
     end
   end
   
   def destroy
     book = Book.find(params[:id])
     if book.destroy
-      flash[:notice] = "successfully"
+      flash[:notice] = "Book was successfully destroyed"
       redirect_to books_path
     else
       render :new
